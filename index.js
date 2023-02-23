eea79391c2e3b8da143d5b3f8dd058e1449abfd3
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-02-23 15:15:01 +0400

    Add index

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 
 // Create GET request
+app.get("/", (req, res) => {
+    res.send("Welcome");
+});
+
 app.get("membership/0x3472059945ee170660a9A97892a3cf77857Eba3A", (req, res) => {
     res.json({
         level: 0, // 0 = Silver, 1 = Gold, 3 = Platinum
