03b5b2bef02821aa51b0340458e1d516deab3859
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-02-26 03:11:23 +0400

    Update memebership to gold

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ app.get("/", (req, res) => {
     res.send("Welcome");
 });
 
-app.get("/membership/0x3472059945ee170660a9A97892a3cf77857Eba3A", (req, res) => {
-    res.json({
-        level: 0, // 0 = Silver, 1 = Gold, 3 = Platinum
-    });
-});
+app.get(
+    "/membership/0x3472059945ee170660a9A97892a3cf77857Eba3A",
+    (req, res) => {
+        res.json({
+            level: 1, // 0 = Silver, 1 = Gold, 3 = Platinum
+        });
+    }
+);
 
 app.listen(5000, () => {
     console.log("Running on port 5000.");
